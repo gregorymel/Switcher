e74e15c6b3e3264353c8223e4088c2cee46d6a26
ArthurSamuelyan <artur.samuelyan@phystech.edu>
2017-10-31 20:34:34 +0300

    Why does mount fail?

--- switcher-core/switch.py
+++ switcher-core/switch.py
@@ -1,9 +1,25 @@
 import manager
 import sys
+import os
 
 def main():
-    mngr = manager.Manager("./control.sock")
-    mngr.mount(id=0, mountpoint="./mount-dir", mode="proxy", proxy_dir="./proxy-dir")
+
+    if len(sys.argv)==3:
+        dir_src = sys.argv[1]
+        dir_dst = sys.argv[2]
+
+        if ( not os.path.isdir( dir_src ) or not os.path.isdir( dir_src ) ):
+            print( "Wrong parameters! Must be <dir_src> <dir_dst>" )
+            sys.exit( 1 )
+
+    
+        default_control_sock = "./control.sock" #"/var/run/fuse_control.sock"
+        default_mountpoint = "./mount-dir" #"/mnt"
+
+        mngr = manager.Manager( default_control_sock )
+        mngr.mount(id=0, mountpoint=default_mountpoint, mode="proxy", proxy_dir=dir_src)
+        mngr.set_mode(id=0, mode="stub")
+        mngr.set_mode(id=0, mode="proxy", proxy_dir=dir_dst)
 
 if __name__ == '__main__':
     main()
